9480f8cf961a8aeba290507572702e14a1f727d4
amdsaad <saadamd@gmail.com>
2021-06-10 02:47:08 +0400

    other pages

---
+++ pages/contact-us/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <section id="aboutHeader" class="header">
+      <h1>Contact us</h1>
+    </section>
+    <section id="mainID">
+      <p>
+        Great Lakes Entry Systems provides help to the disabled by specializing
+        in custom designed wheelchair ramps, stairs, steps and platforms. We
+        offer permanent and temporary systems for both commercial and
+        residential applications.
+      </p>
+      <br />
+      <p>
+        Great Lakes Entry Systems is based on the principles of quality,
+        customer satisfaction, honesty, and integrity. It is our mission to not
+        only determine what your needs are, but to do everything we can to meet
+        or exceed those needs. Our team based approach allows us to continuously
+        improve our services while working toward the goal of being known as the
+        most professional, dependable, and efficient supplier in the industry.
+      </p>
+      <br />
+      <p>
+        With our two decades of design and manufacturing experience, combined
+        with a 100,000 square foot manufacturing facility in Quincy, Michigan,
+        Great Lakes Entry Systems has both the expertise to insure quality and
+        the commitment to stand behind our products for generations to come.
+      </p>
+      <h2>Great Lakes Entry Systems</h2>
+      <p>
+        87 Taylor Street <br />
+        Quincy, Michigan 49082
+      </p>
+      <br />
+      <p>(517) 639 7402</p>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
