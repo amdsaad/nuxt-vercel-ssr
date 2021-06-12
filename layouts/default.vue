77aff66294f2d8b8f884d14959aecbf3ce45c3f8
amdsaad <saadamd@gmail.com>
2021-06-12 19:50:57 +0400

    copy right

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,5 +3,19 @@
     <navbar />
     <Nuxt />
     <app-footer />
+    <footer>
+      <div class="containerMn">
+        <div class="copyright">
+          Copyright &copy; 2014-
+          <script>
+            document.write(new Date().getFullYear());
+          </script>
+          Great Lakes Entry | Website developed & hosted by
+          <a href="https://schrader.co" target="blank"
+            >Schrader Web Solutions</a
+          >
+        </div>
+      </div>
+    </footer>
   </div>
 </template>
